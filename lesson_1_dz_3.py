f383927bbcda6c7c3f33d3d1ecc36cba83c24c7d
Aliaksei_Litvintsa <89184501+LaseStar@users.noreply.github.com>
2021-11-06 05:40:37 +0300

    Update lesson_1_dz_3.py

--- lesson_1_dz_3.py
+++ lesson_1_dz_3.py
@@ -2,13 +2,14 @@
 # Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
 
 # Создаем список из слов
-percent_list = [' процент ', 'процента', 'процентов']
+percent_list = ['процент ', 'процента', 'процентов']
 
 # прогоняем список от 1 до 100 и выводим результат
 for i in range(1, 101):
-    if i == 1:
+    div = i % 10
+    if div == 1:
         name_percent = percent_list[0]
-    elif i < 5:
+    elif div < 5:
         name_percent = percent_list[1]
     else:
         name_percent = percent_list[2]
