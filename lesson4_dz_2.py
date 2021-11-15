71a60f05a75ba3a2e818b0207a9ab27c79d1a467
Litvincev Flekse <lase_star@mail.ru>
2021-11-15 12:56:44 +0300

    add_dz_2

---
+++ lesson4_dz_2.py
@@ -0,0 +1,32 @@
+from requests import get
+# from decimal import Decimal
+
+
+def currency_rates(currency_code):
+    currency_code = currency_code.upper()
+    # обращаемся к нашему url
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    res = get(url)
+    if not res:
+        print('Response Failed')
+        print(res.status_code)
+        return None
+    # Выводим сообщение что мы подключились
+    print('Response OK')
+
+    rate_text = res.text
+    # Проверяем если вообще такая валюта
+    if rate_text.find(currency_code) == -1:
+        print(f'Валюта {currency_code} не найдена')
+        return None
+
+    _rate = rate_text[rate_text.find(currency_code):]
+    rate = (_rate[_rate.find('<Value>')+len('<Value>'):_rate.find('</Value>')])
+    rate_float = float(rate.replace(',', '.'))
+    # rate_decimal = Decimal(rate_float)
+    return print(f'Курс {currency_code} : {rate_float}')
+
+
+if __name__ == '__main__':
+    currency_rates('byn')
+    currency_rates('usd')
