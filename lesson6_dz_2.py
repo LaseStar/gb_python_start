e603dce35b8a9b5d325921714e780964f8a14393
Litvincev Flekse <lase_star@mail.ru>
2021-11-23 15:02:20 +0300

    add_dz_2

---
+++ lesson6_dz_2.py
@@ -0,0 +1,22 @@
+#  (вместо 1) Найти IP адрес спамера и количество отправленных им запросов по данным файла логов
+#  из предыдущего задания.
+# Примечания: спамер — это клиент, отправивший больше всех запросов; код должен работать даже с файлами,
+# размер которых превышает объем ОЗУ компьютера.
+
+list_line = []
+list_ip = []
+with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        l_split = line.split()
+        list_ip.append(l_split[0])
+
+set_ip = set(list_ip)
+meeting = 0
+ip = ''
+for i in set_ip:
+    meeting_ = list_ip.count(i)
+    if meeting_ > meeting:
+        meeting = meeting_
+        ip = i
+
+print('спамер ip: ', ip, 'встречается ', meeting)
